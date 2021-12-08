08b8456a1f6a95a75bd161498a813f908c1a6e0a
jeetmistry <jeetmistry26@gmail.com>
2021-12-08 10:16:56 +0530

    Day 1

---
+++ Basics of Programming/Getting Started With Java/PrintZ.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class PrintZ{
+    public static void main(String[] args) {
+        //Printing Z using spacing 
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DSA
+# Data Structures and Algorithm Practice
+
+#Day 1 :
+1. Print Z
+2. Grading System
