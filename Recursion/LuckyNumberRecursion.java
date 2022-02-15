0d754e430cc739f15aba2f4d8eeddb2f3172825d
jeetmistry <jeetmistry26@gmail.com>
2022-02-15 13:17:19 +0530

    Lucky Number Recursion

---
+++ Recursion/LuckyNumberRecursion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LuckyNumberRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(check(n,2));
+    }
+
+    private static boolean check(int n, int counter) {
+        if(n<counter){
+            return true;
+        }
+        if(n%counter==0){
+            return false;
+        }
+        n-=n/counter;
+        counter++;
+        return check(n,counter);
+    }
+}
